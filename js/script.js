1d85c0788778c727b3507b943de45023941fb40b
Konstantin Borodkin <kongreat13@gmail.com>
2020-10-09 20:57:21 +0600

    several fixes in e and d

--- js/script.js
+++ js/script.js
@@ -4,17 +4,27 @@ let ang = document.getElementById("ang");
 let images = document.getElementById("images");
 
 // //task 1: a, b
-function loadImage(success, error){
-    js.onload = success;
-    js.onerror = error;
-}
 
-loadImage(() =>{
-    console.log("JS icon loaded successfully");
-}, 
-    () => {
-    console.log("JS icon loading failed");
-});
+// function loadImage(success, error){
+//     js.onload = success;
+//     js.onerror = error;
+// }
+
+// function loadImage(success, error){
+//     let img = document.createElement('img')
+//     img.src = "img/js.png"
+//     images.appendChild(img)
+
+//     img.onload = success;
+//     img.onerror = error;
+// }
+
+// loadImage(() =>{
+//     console.log("JS icon loaded successfully");
+// }, 
+//     () => {
+//     console.log("JS icon loading failed");
+// });
 
 // //****************************** ***************************************/
 
@@ -36,6 +46,17 @@ loadImage(() =>{
 //     })
 // }
 
+// function loadImage(){
+//     return new Promise(function(resolve, reject){
+//         let img = document.createElement('img')
+//         img.src = "img/js.png"
+//         images.appendChild(img)
+
+//         img.onload = resolve
+//         img.onerror = reject
+//     })
+// }
+
 // let promise = loadImage();
 // promise.then(success, error);
 
@@ -48,21 +69,31 @@ loadImage(() =>{
 //     console.log("Js icon loaded successfully");
 // }
 
-// function error(){
-//     console.log("Js icon loading failed");
-// }
 
 // async function loadImage(){
-//     return await new Promise((resolve, reject) => {
-//         js.onload = resolve;
-//         js.onerror = reject;
+//      await new Promise((resolve, reject) => {
+//         let img = document.createElement('img')
+//         img.src = "img/js.png"
+//         images.appendChild(img)
+
+//         img.onload = resolve;
+//         img.onerror = () => reject(new Error("failed to load"));
         
 //     });
 // }
 
+// // async function loadImage(){
+// //     await new Promise((resolve, reject) => {
+// //        js.onload = resolve;
+// //        js.onerror = () => reject(new Error("failed to load"));
+       
+// //    });
+// // }
+
 
 // let promise = loadImage();
-// promise.then(success, error).then(()=>{console.log("Continue")});
+// promise.then(success, null).then(()=>{console.log("Continue")});
+
 
 
 //******************************************************************************* */
@@ -86,4 +117,47 @@ loadImage(() =>{
 // }
 
 // loadImage("img/js.png").then(()=>{return loadImage("img/ts.png")}).then(()=>{return loadImage("img/angular.png")}).then(()=>{allSuccess()});
+
+
+
+
+
+//e2
+
+// function success(){
+//     console.log("Js icon loaded successfully");
+//     c++
+// }
+
+// function error(){
+//     console.log("Js icon loading failed");
+// }
+
+// function allSuccess(){
+//     if(c==3){
+//         console.log("All icons loaded");
+//     }
+
+    
+// }
+
+
+// async function loadImage(url){
+//     return await new Promise((resolve, reject) =>{
+//         let img = new Image();
+//         img.onload = () => resolve(img);
+//         img.onerror = () => reject(new Error("failed to load"));
+//         img.src = url;
+//         images.appendChild(img);
+//     });
+// }
+
+// let c = 0
+
+// loadImage("img/js.png").then(success).then(()=> console.log("First")).then(allSuccess)
+// loadImage("img/ts.png").then(success).then(()=> console.log("Second")).then(allSuccess)
+// loadImage("img/angular.png").then(success).then(()=> console.log("Third")).then(allSuccess)
+
+
+
 
